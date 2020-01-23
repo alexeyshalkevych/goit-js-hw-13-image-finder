00755d94cdfb358792cba77ac1bbd75f1ac1d7e2
Alexey <valnistuy@gmail.com>
2020-01-24 01:07:41 +0200

    create class imageApp

--- src/js/img-finder.js
+++ src/js/img-finder.js
@@ -3,75 +3,183 @@ import searchFormTemplate from '../templates/search-form.hbs';
 import galleryListTemplate from '../templates/gallery-list.hbs';
 import photoCardItemsTemplate from '../templates/photo-card-items.hbs';
 import loadMoreButtonTemplate from '../templates/load-button.hbs';
+import searchImagesAppTemplate from '../templates/search-image-app.hbs';
 
-const refs = {
-  searchImagesApp: document.querySelector('.js-app'),
-  // searchForm: document.querySelector('#search-form'),
-  // galleryList: document.querySelector('.js-gallery'),
-  // loadMoreBtn: document.querySelector('button[data-action="load-more"]'),
-};
+class imageApp {
+  constructor() {
+    this.body = null;
+    this.searchForm = null;
+    this.imageList = null;
+    this.loadMoreBtn = null;
+    this.app = null;
+    this.inputValue = null;
 
-createSearchInput();
-createImageList();
+    this.init();
+    this.addEvent();
+  }
 
-const testSearchForm = document.querySelector('#search-form');
-const testImageList = document.querySelector('.js-gallery');
-// refs.loadMoreBtn.addEventListener('click', )
-testSearchForm.addEventListener('submit', testHandleSubmit);
+  init() {
+    this.getDom();
 
-function testHandleSubmit(event) {
-  event.preventDefault();
+    // this.createDomElement(this.body, searchImagesAppTemplate());
+    // this.createDomElement(this.body, searchImagesAppTemplate());
+    this.createDomElement(this.app, searchFormTemplate());
+    this.createDomElement(this.app, galleryListTemplate());
 
-  const inputValue = event.currentTarget.elements.query.value;
+  }
 
-  clearListItems();
-  imagesService.resetPage();
-  imagesService.searchQuery = inputValue;
+  getDom() {
+    // this.body = document.querySelector('body');
+    this.app = document.querySelector('.js-app');
+    this.searchForm = document.querySelector('#search-form');
+    this.imageList = document.querySelector('.js-gallery');
+    this.loadMoreBtn = document.querySelector(
+      'button[data-action="load-more"]',
+    );
+    console.log(this.body);
+    console.log(this.app);
+    console.log(this.searchForm);
+    console.log(this.imageList);
+    console.log(this.loadMoreBtn);
+  }
 
-  axiosImages();
-}
+  createDomElement(insertElem, element) {
+    insertElem.insertAdjacentHTML('beforeend', element);
+  }
 
-function axiosImages() {
-  imagesService
-    .axiosImages()
-    .then(data => {
-      insertListItems(data.hits);
+  // createDom() {
+  //   this.body.insertAdjacentHTML('beforeend', searchImagesAppTemplate());
+  //   this.app.insertAdjacentHTML('beforeend', searchFormTemplate());
+  //   this.app.insertAdjacentHTML('beforeend', galleryListTemplate());
+  // }
 
-      createBtnLoadMore();
+  addEvent() {
+    this.searchForm.addEventListener('submit', this.handlerSubmit);
+  }
 
-      const loadMoreBtn = document.querySelector(
-        'button[data-action="load-more"]',
+  handlerSubmit(event) {
+    event.preventDefault();
+
+    this.input = event.currentTarget.elements.query;
+
+    this.clearListItems();
+
+    imagesService.resetPage();
+    imagesService.searchQuery = this.input.value;
+
+    this.axiosImages();
+
+    this.input.value = '';
+  }
+
+  loadMoreButtonHadlerCLick() {
+    this.axiosImages();
+  }
+
+  axiosImages() {
+    imagesService
+      .axiosImages()
+      .then(data => {
+        this.insertListItems(data.hits);
+      })
+      .catch(console.error);
+  }
+
+  insertListItems(item) {
+    if (!this.imageList.children.length) {
+      this.createDomElement(this.app, loadMoreButtonTemplate());
+
+      this.loadMoreBtn.addEventListener(
+        'click',
+        this.loadMoreButtonHadlerCLick,
       );
+    }
 
-      loadMoreBtn.addEventListener('click', loadMoreButtonHadlerCLick);
-    })
-    .catch(console.error);
+    this.createDomElement(this.imageList, photoCardItemsTemplate(item));
+  }
+
+  clearListItems() {
+    this.imageList.innerHTML = '';
+  }
 }
 
-function loadMoreButtonHadlerCLick() {
-  axiosImages();
-}
+new imageApp();
 
-function insertListItems(item) {
-  const markup = photoCardItemsTemplate(item);
-  testImageList.insertAdjacentHTML('beforeend', markup);
-}
+// const refs = {
+//   searchImagesApp: document.querySelector('.js-app'),
+//   // searchForm: document.querySelector('#search-form'),
+//   // galleryList: document.querySelector('.js-gallery'),
+//   // loadMoreBtn: document.querySelector('button[data-action="load-more"]'),
+// };
 
-function createSearchInput() {
-  refs.searchImagesApp.insertAdjacentHTML('beforeend', searchFormTemplate());
-}
+// createSearchInput();
+// createImageList();
 
-function createImageList() {
-  refs.searchImagesApp.insertAdjacentHTML('beforeend', galleryListTemplate());
-}
+// const testSearchForm = document.querySelector('#search-form');
+// const testImageList = document.querySelector('.js-gallery');
 
-function createBtnLoadMore() {
-  refs.searchImagesApp.insertAdjacentHTML(
-    'beforeend',
-    loadMoreButtonTemplate(),
-  );
-}
+// console.log(testSearchForm);
 
-function clearListItems() {
-  testImageList.innerHTML = '';
-}
+// testSearchForm.addEventListener('submit', testHandleSubmit);
+
+// function testHandleSubmit(event) {
+//   event.preventDefault();
+
+//   const form = event.currentTarget;
+//   const input = form.elements.query;
+
+//   clearListItems();
+//   imagesService.resetPage();
+//   imagesService.searchQuery = input.value;
+
+//   axiosImages();
+
+//   input.value = '';
+// }
+
+// function axiosImages() {
+//   imagesService
+//     .axiosImages()
+//     .then(data => {
+//       insertListItems(data.hits);
+//     })
+//     .catch(console.error);
+// }
+
+// function loadMoreButtonHadlerCLick() {
+//   axiosImages();
+// }
+
+// function insertListItems(item) {
+//   if (!testImageList.children.length) {
+//     createBtnLoadMore();
+
+//     const loadMoreBtn = document.querySelector(
+//       'button[data-action="load-more"]',
+//     );
+
+//     loadMoreBtn.addEventListener('click', loadMoreButtonHadlerCLick);
+//   }
+
+//   const markup = photoCardItemsTemplate(item);
+//   testImageList.insertAdjacentHTML('beforeend', markup);
+// }
+
+// function createSearchInput() {
+//   refs.searchImagesApp.insertAdjacentHTML('beforeend', searchFormTemplate());
+// }
+
+// function createImageList() {
+//   refs.searchImagesApp.insertAdjacentHTML('beforeend', galleryListTemplate());
+// }
+
+// function createBtnLoadMore() {
+//   refs.searchImagesApp.insertAdjacentHTML(
+//     'beforeend',
+//     loadMoreButtonTemplate(),
+//   );
+// }
+
+// function clearListItems() {
+//   testImageList.innerHTML = '';
+// }
